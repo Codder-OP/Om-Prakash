a575778f373dd5280f3b1fdbb3604cb3925f5464
OM PRAKASH <49099068+Codder-OP@users.noreply.github.com>
2021-04-01 08:25:25 +0530

    Add files via upload

---
+++ React/AllAssignments/src/CRUD/Data.js
@@ -0,0 +1,15 @@
+
+const userList =[
+{
+    id:1,
+    name: 'Om Prakash',
+    username: 'op12345'
+},
+{
+    id:2,
+    name: 'Ram',
+    username: 'Ram12345'
+}
+];
+
+export default userList;
---
+++ React/AllAssignments/src/components/Employee.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Employee() {
+    return (
+       <table>
+
+            <tr>
+                <th colSpan="2" >
+                    <td>FirstName</td>
+                    <td>LastName</td>
+                </th>
+            </tr>
+            <tr>
+                <td>Om</td>
+                <td>Prakash</td>
+            </tr>
+            <tr>
+                <td>Ram</td>
+                <td>Kumar</td>
+            </tr>
+            <tr>
+                <td>Capiton</td>
+                <td>America</td>
+            </tr>
+            <tr>
+                <td>Iron</td>
+                <td>Man</td>
+            </tr>
+        </table>
+        
+    )
+}
+
+export default Employee
